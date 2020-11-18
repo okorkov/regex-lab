110ccf279081a54b0aadd6b1a74994e6d2a01a3a
okorkov <alex.okorkov@gmail.com>
2020-11-18 12:48:49 -0800

    commit tree 192249d8df908e592f2570164a335b525ab78d3a

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
+    word[/(?i)\A[aeiou]/] != nil
+ end
 
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-
+ def words_starting_with_un_and_ending_with_ing(text)
+    text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/\A[A-Z].*\W$/) != nil
 end
 
 def valid_phone_number?(phone)
-
-end
+    phone.scan(/[0-9]/).join('').length == 10
+  end
